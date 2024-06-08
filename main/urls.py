d9a87feefc7144d5bc5d49acce7b54144c90031b
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-08 09:09:59 +0545

    404 URL Created

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     # path('',views.home,name='home'),
     # path('home/',views.home,name='home')
+    path('404/',views.custom404,name='custom404'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+
+def custom404(request):
+    return render(request,'404.html')
