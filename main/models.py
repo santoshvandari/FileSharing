5b11cbc5fff7e7f414ca355f8f3340e7d54ca987
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-06-09 11:30:32 +0545

    Media Folder Added and Updated the Setting

--- main/models.py
+++ main/models.py
@@ -3,4 +3,14 @@ from django.db import models
 # Create your models here.
 
 class SharedFiles(models.Model):
-    pass
+    file = models.FileField(upload_to='uploads/')
+    upload_time = models.DateTimeField(auto_now_add=True)
+    expiration_time = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.expiration_time:
+            self.expiration_time = timezone.now() + timedelta(hours=24)
+        super().save(*args, **kwargs)
+
+    def is_expired(self):
+        return timezone.now() > self.expiration_time
